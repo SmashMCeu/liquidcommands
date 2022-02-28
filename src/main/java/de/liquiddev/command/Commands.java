22bb4fa365e9d3a2b5d507cd2acf9c24ac659a0b
Sneakometer <sneakometer@gmail.com>
2022-02-28 18:08:00 +0100

    :fire: Remove this

--- src/main/java/de/liquiddev/command/Commands.java
+++ src/main/java/de/liquiddev/command/Commands.java
@@ -5,13 +5,10 @@ public class Commands {
 	private static volatile String defaultPrefix = "§bLiquidCommand §8» ";
 
 	public static String getDefaultPrefix() {
-		System.out.println("returning default prefix " + defaultPrefix);
-		System.out.println("classpath from commands: " + Commands.class.getName());
 		return defaultPrefix;
 	}
 
 	public static void setDefaultPrefix(String prefix) {
-		System.out.println("set default prefix to " + prefix);
 		defaultPrefix = prefix;
 	}
 }
