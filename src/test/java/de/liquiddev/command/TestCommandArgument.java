38b8906fc1272a99226e411fac7e230e553b9b92
Packsolite <packsolite@gmail.com>
2022-05-12 20:34:01 +0200

    :white_check_mark: Fix command argument tests

--- src/test/java/de/liquiddev/command/TestCommandArgument.java
+++ src/test/java/de/liquiddev/command/TestCommandArgument.java
@@ -10,10 +10,15 @@ import org.junit.jupiter.api.Test;
 
 public class TestCommandArgument {
 
+	static CommandArguments argumentsOf(CommandNode<?> command, String... strings) {
+		AbstractCommandSender<?> sender = mock(AbstractCommandSender.class);
+		return CommandArguments.fromStrings(command, sender, strings);
+	}
+
 	@Test
 	public void testIsPresent() {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "test" });
+		CommandArguments args = argumentsOf(command, "test");
 		assertTrue(args.isPresent(0));
 		assertFalse(args.isPresent(1));
 	}
@@ -21,63 +26,63 @@ public class TestCommandArgument {
 	@Test
 	public void testCheck() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "test" });
+		CommandArguments args = argumentsOf(command, "test");
 		assertTrue(args.check(0, "test"));
 	}
 
 	@Test
 	public void testCheckIgnoreCase() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "test" });
+		CommandArguments args = argumentsOf(command, "test");
 		assertTrue(args.check(0, "TEST"));
 	}
 
 	@Test
 	public void testCheckWithCase() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "test" });
+		CommandArguments args = argumentsOf(command, "test");
 		assertFalse(args.checkWithCase(0, "TEST"));
 	}
 
 	@Test
 	public void testCheckFail() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "test" });
+		CommandArguments args = argumentsOf(command, "test");
 		assertFalse(args.check(0, "no"));
 	}
 
 	@Test
 	public void testCheckException() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] {});
+		CommandArguments args = argumentsOf(command);
 		assertThrows(MissingCommandArgException.class, () -> assertFalse(args.check(0, "no")));
 	}
 
 	@Test
 	public void testCheckOptional() {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "test" });
+		CommandArguments args = argumentsOf(command, "test");
 		assertTrue(args.checkOptional(0, "test"));
 	}
 
 	@Test
 	public void testCheckOptionalFail() {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] {});
+		CommandArguments args = argumentsOf(command, "test");
 		assertFalse(args.checkOptional(0, "no"));
 	}
 
 	@Test
 	public void testCheckOptionalFailIndex() {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] {});
+		CommandArguments args = argumentsOf(command);
 		assertFalse(args.checkOptional(0, "test"));
 	}
 
 	@Test
 	public void testNext() {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "prevarg", "command", "arg0" });
+		CommandArguments args = argumentsOf(command, "prevarg", "command", "arg0");
 		args.next(command, 2);
 		assertEquals(1, args.length());
 	}
@@ -85,7 +90,7 @@ public class TestCommandArgument {
 	@Test
 	public void testPrevArgs() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "prevarg", "command", "arg0" });
+		CommandArguments args = argumentsOf(command, "prevarg", "command", "arg0");
 		args.next(command, 2);
 		assertTrue(args.isPresent(-1));
 		assertEquals("prevarg", args.get(-1));
@@ -94,7 +99,7 @@ public class TestCommandArgument {
 	@Test
 	public void testPrevArgsOutOfBounds() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "prevarg", "command", "arg0" });
+		CommandArguments args = argumentsOf(command, "prevarg", "command", "arg0");
 		args.next(command, 1);
 		assertThrows(IndexOutOfBoundsException.class, () -> args.get(-2));
 	}
@@ -102,7 +107,7 @@ public class TestCommandArgument {
 	@Test
 	public void testCheckOptionalPrevArgs() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "prevarg", "commandname", "arg0" });
+		CommandArguments args = argumentsOf(command, "prevarg", "commandname", "arg0");
 		args.next(command, 2);
 		assertTrue(args.checkOptional(-1, "prevarg"));
 	}
@@ -110,7 +115,7 @@ public class TestCommandArgument {
 	@Test
 	public void testCheckOptionalPrevArgsNegative() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "prevarg", "commandname", "arg0" });
+		CommandArguments args = argumentsOf(command, "prevarg", "commandname", "arg0");
 		args.next(command, 2);
 		assertFalse(args.check(-1, "commandname"));
 	}
@@ -118,7 +123,7 @@ public class TestCommandArgument {
 	@Test
 	public void testCheckOptionalPrevArgsOutOfBounds() throws MissingCommandArgException {
 		CommandNode<?> command = mock(CommandNode.class);
-		CommandArguments args = CommandArguments.fromStrings(command, null, new String[] { "prevarg", "commandname", "arg0" });
+		CommandArguments args = argumentsOf(command, "prevarg", "commandname", "arg0");
 		args.next(command, 2);
 		assertFalse(args.checkOptional(-2, "prevargs"));
 	}
